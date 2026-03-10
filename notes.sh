2ec247caab688e987f4d6619147a65fb543c8461
Lokendra Pandit <lokendra.pandit@gmail.com>
2026-03-10 09:48:53 +0000

    username and password

---
+++ notes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+read -p "Enter your name : " username
+read -p "Enter your email : " useremail
+read -p "Enter your password : " userpwd
+echo -e "Hello $username\n$useremail\n$userpwd"
